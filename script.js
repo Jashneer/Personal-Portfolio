69230ca6d9df901916ef661a426c7f27246f6d82
Jashneer <jashneerkaur9@gmail.com>
2025-05-12 22:19:23 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// function toggleMenu() {
+//     const menu = document.querySelector(".menu-links");
+//     const icon = document.querySelector(".hamburger-icon");
+//     menu.classList.toggle("open");
+//     icon.classList.toggle("open");
+    
+// }
+
+// $(document).ready(function () { $('.navbar-toggler').click(function () { $('#sidebar').toggleClass('show'); }); });
+
+// typing animation
+var typed = new Typed(".typing",{
+    strings:["", "Aspiring Developer"],
+    typespeed:100,
+    BackSpeed: 60,
+    loop: true
+
+})
